9635e90e52a22b16aae6409e0e0a94b55f63bd3c
jsevamo <j_sevamo@hotmail.com>
2020-01-28 12:21:59 -0500

    Started commenting

--- Main.py
+++ Main.py
@@ -1,14 +1,26 @@
+# /*******************************************************
+# * 2020 Juan Sebastián Vargas Molano j.sevamo@gmail.com
+# *******************************************************/
+
+
+# Main function for the raytracer
 def main():
+    # This is how we can create a ppm image to write.
     outputImage = open("renderedImage.ppm", "w+")
 
+    # width (nx) and height (ny) of the output image.
     nx: int = 200
     ny: int = 100
 
+    # create a ppm image header based on this: https://en.wikipedia.org/wiki/Netpbm#File_formats
     print("P3\n" + str(nx) + " " + str(ny) + "\n255\n")
     outputImage.write("P3\n" + str(nx) + " " + str(ny) + "\n255\n")
 
+    # The for loop that writes the pixels of the image. It writes from left to right
+    # and then from top to bottom.
     for j in range(ny, 0, -1):
         for i in range(0, nx, 1):
+            #Just random numbers for now to create an image.
             r: float = i / nx
             g: float = j / ny
             b: float = 0.2
@@ -18,6 +30,8 @@ def main():
             print(str(ir) + " " + str(ig) + " " + str(ib) + "\n")
             outputImage.write(str(ir) + " " + str(ig) + " " + str(ib) + "\n")
 
+    # Makes sure to close the output image.
     outputImage.close()
+
 
 main()
