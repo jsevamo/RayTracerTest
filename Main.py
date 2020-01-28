575b852775e598c59eda88fee1f51d53b939e21a
jsevamo <j_sevamo@hotmail.com>
2020-01-28 12:14:54 -0500

    Creates "renderedImage.ppm" everytime the program runs

--- Main.py
+++ Main.py
@@ -1,10 +1,13 @@
 def main():
+    outputImage = open("renderedImage.ppm", "w+")
+
     nx: int = 200
     ny: int = 100
 
     print("P3\n" + str(nx) + " " + str(ny) + "\n255\n")
+    outputImage.write("P3\n" + str(nx) + " " + str(ny) + "\n255\n")
 
-    for j in range(ny - 1, 0, -1):
+    for j in range(ny, 0, -1):
         for i in range(0, nx, 1):
             r: float = i / nx
             g: float = j / ny
@@ -13,6 +16,8 @@ def main():
             ig: int = int(255.99 * g)
             ib: int = int(255.99 * b)
             print(str(ir) + " " + str(ig) + " " + str(ib) + "\n")
+            outputImage.write(str(ir) + " " + str(ig) + " " + str(ib) + "\n")
 
+    outputImage.close()
 
 main()
