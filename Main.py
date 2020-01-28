071cc6cc5779ed6ce8786b2b66fe65506729878b
jsevamo <j_sevamo@hotmail.com>
2020-01-28 12:06:37 -0500

    Prints a ppm image in the Main function

--- Main.py
+++ Main.py
@@ -1,5 +1,18 @@
 def main():
-    print("hello word")
+    nx: int = 200
+    ny: int = 100
+
+    print("P3\n" + str(nx) + " " + str(ny) + "\n255\n")
+
+    for j in range(ny - 1, 0, -1):
+        for i in range(0, nx, 1):
+            r: float = i / nx
+            g: float = j / ny
+            b: float = 0.2
+            ir: int = int(255.99 * r)
+            ig: int = int(255.99 * g)
+            ib: int = int(255.99 * b)
+            print(str(ir) + " " + str(ig) + " " + str(ib) + "\n")
 
 
 main()
