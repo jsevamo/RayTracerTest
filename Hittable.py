c90c11af6a162118595a96ba2e2d5477dfe7b990
GIM UMNG <gim@unimilitar.edu.co>
2020-02-06 14:32:45 -0500

    Let's do this again. HitRecord and Hittable classes done.

--- Hittable.py
+++ Hittable.py
@@ -1,18 +1,19 @@
 # from dataclasses import dataclass
 from Ray import Ray as ray
 from Vec3 import Vec3 as vec3
+from abc import ABC, abstractmethod
 
 
 # @dataclass
 class Hit_Record:
-    t: float
-    p: vec3
-    normal: vec3
+    def __init__(self, t: float = None, p: vec3 = None, normal: vec3 = None):
+        self.t = t
+        self.p = p
+        self.normal = normal
 
 
-# noinspection PyMethodParameters
-class Hittable:
+class Hittable(ABC):
 
-    def Hit(self, r: ray, t_min: float, t_max: float, rec: Hit_Record):
+    @abstractmethod
+    def Hit(self, r: ray, t_min: float, t_max: float, rec: Hit_Record) -> bool:
         pass
-
